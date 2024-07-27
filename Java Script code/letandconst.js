0d30cedc466ad17fdcc2a048f154e25182ea282a
ijazdev101 <ijazkhan19932@gmail.com>
2024-07-27 21:07:41 +0300

    let and const
    let and const

--- Java Script code/letandconst.js
+++ Java Script code/letandconst.js
@@ -8,4 +8,10 @@ accountCity = "lahore"
 //accountId =2; error we cant change const
 console.log(accountId);
 //amazing print in a table
+//const accountId = 101 same name const cant be change
+ accountEmail = "abbas@gmail.com";
+accountPassword ="9091";
+accountCity = "Karachi"
 console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
