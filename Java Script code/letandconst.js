365e4a40aa4c970a8618be7b0d6d5d271fd6b504
ijazdev101 <ijazkhan19932@gmail.com>
2024-07-27 21:03:19 +0300

    table
    table

---
+++ Java Script code/letandconst.js
@@ -0,0 +1,11 @@
+console.log("Let and Const");
+
+const accountId = 101
+let accountEmail = "ijazkhan19932@gmail.com";
+var accountPassword ="12345";
+accountCity = "lahore"
+
+//accountId =2; error we cant change const
+console.log(accountId);
+//amazing print in a table
+console.table([accountId,accountEmail,accountPassword,accountCity]);
--- Java Script code/test.js
+++
@@ -1,1 +0,0 @@
-console.log("ijaz khan");
